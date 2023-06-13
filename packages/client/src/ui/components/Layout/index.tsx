3596e567e57acd3ed1b678c0a60e97c42df2b18e
iDenisOff <andreyfrolovdda@gmail.com>
2023-06-13 16:56:49 +0300

    Linter settings

--- packages/client/src/ui/components/Layout/index.tsx
+++ packages/client/src/ui/components/Layout/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './style.scss'
+import React from 'react';
+import './style.scss';
 
-// TODO: There is PropsWithChildren in React types to define comp with children
 type TLayoutProps = {
-  children: React.ReactNode
-}
-const Layout: React.FC<TLayoutProps> = ({ children }) => {
-  return <div className="layout">{children}</div>
-}
+    children: React.ReactNode;
+};
 
-export default Layout
+const Layout: React.FC<TLayoutProps> = ({ children }) => {
+    return <div className="layout">{children}</div>;
+};
+
+export default Layout;
